36dda470a15a7a06d3957a320498596c45447ff4
wjdan94 <walharth@bu.edu>
2017-10-13 13:15:00 -0400

    Added NumNodes to deploy scripts

--- large.sh
+++ large.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
-
 set -x
-#kh clean
+export THREADS=${1:-1}
+export ITERS=${2:-1}
+export NODES=${3:-1}
+export RUN_DEBUG=${DEBUG:-0}
 
-#./hosted/build/Debug/reconstruction -o 3TReconstruction.nii -i data/14_3T_nody_001.nii data/10_3T_nody_001.nii data/21_3T_nody_001.nii data/23_3T_nody_001.nii -m data/mask_10_3T_brain_smooth.nii --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 1.0 --debug 0 --numThreads $1 --useCPU --iterations $2
-
-./build/reconstruction -o 3TReconstruction.nii -i data/14_3T_nody_001.nii data/10_3T_nody_001.nii data/21_3T_nody_001.nii data/23_3T_nody_001.nii -m data/mask_10_3T_brain_smooth.nii --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 1.0 --debug 0 --numThreads $1 --useCPU --iterations $2
-
+./build/reconstruction -o 3TReconstruction.nii -i data/14_3T_nody_001.nii data/10_3T_nody_001.nii data/21_3T_nody_001.nii data/23_3T_nody_001.nii -m data/mask_10_3T_brain_smooth.nii --disableBiasCorrection --useAutoTemplate --useSINCPSF --resolution 2.0 --debug $RUN_DEBUG --numThreads $THREADS --useCPU --iterations $ITERS --numNodes $NODES
 
 
